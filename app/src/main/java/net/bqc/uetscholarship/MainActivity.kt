668048a37a1c2080d2cceae0930fb513bbea8d64
bqcuong <bqcuong2212@gmail.com>
2017-09-09 16:48:51 +0700

    disable subscribe button

--- app/src/main/java/net/bqc/uetscholarship/MainActivity.kt
+++ app/src/main/java/net/bqc/uetscholarship/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,22 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // setup toolbar
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+//        setSupportActionBar(toolbar)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item!!.itemId) {
-            R.id.action_favorite -> {
-                Toast.makeText(this, "Subscribed!", Toast.LENGTH_LONG).show()
-                return true
-            }
-        }
-        return true
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        when (item!!.itemId) {
+//            R.id.action_favorite -> {
+//                FirebaseMessaging.getInstance().subscribeToTopic(getString(R.string.scholarship_topic))
+//                Toast.makeText(this, getString(R.string.msg_subscribed), Toast.LENGTH_LONG).show()
+//                return true
+//            }
+//        }
+//        return true
+//    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.main_menu, menu)
+//        return true
+//    }
 }

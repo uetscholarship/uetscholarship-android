84ddacb6a508f091db91b7553b2edf179eb8c9cf
bqcuong <bqcuong2212@gmail.com>
2017-09-09 16:20:04 +0700

    add subscribe button

--- app/src/main/java/net/bqc/uetscholarship/MainActivity.kt
+++ app/src/main/java/net/bqc/uetscholarship/MainActivity.kt
@@ -2,11 +2,34 @@ package net.bqc.uetscholarship
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // setup toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.action_favorite -> {
+                Toast.makeText(this, "Subscribed!", Toast.LENGTH_LONG).show()
+                return true
+            }
+        }
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
